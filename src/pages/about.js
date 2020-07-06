304d8a65c9dd0768ccf6c13d513498f02a034e20
RC-Atkins <kiratk7147@students.ecpi.edu>
2020-07-06 10:35:58 -0400

    Initial Check-in

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+       <Header headerText="It's pretty cool" />
+       <Header headerText="About Gatsby" />
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
